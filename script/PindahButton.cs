bd8d287b98d9ff9fdc57fcc1e82474a6ab8c1095
Dmarcellakbar <77231005+Dmarcellakbar@users.noreply.github.com>
2022-01-02 15:06:26 +0700

    Add files via upload

---
+++ script/PindahButton.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class PindahButton : MonoBehaviour
+{
+    public void Scene1()
+    {
+        SceneManager.LoadScene("UIMenu");
+    }
+    public void Scene2()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+    public void Scene3()
+    {
+        SceneManager.LoadScene("AboutUs");
+    }
+}
